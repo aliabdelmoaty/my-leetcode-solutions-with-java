fe737d9a45b7c968abd39744b8c36f43ffa29b4b
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-03-01 14:20:10 +0200

    add stack

--- stack.java
+++ stack.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class stack {
     Node top;
     
@@ -5,21 +7,64 @@ public class stack {
         top = null;
     }
     public void push(int item){
+        Node newNode = new Node();
+        newNode.data = item;
         if (isEmpty()) {
+            newNode.next = null;
+            top =newNode;
+        }else{
             
+            newNode.next =top;
+            top =newNode;
         }
     }
    
-    public int pop(){} 
-    public void display(){}
+    public int pop(){
+        int value;
+        Node delete =top;
+        value =top.data;
+        top =top.next;
+        delete=null;
+        return value;
+    } 
+    public void display(){
+       Node temp =top;
+       while (temp != null) {
+        System.out.println(temp.data);
+        temp=temp.next;
+       }
+    }
     public boolean isEmpty(){
         if (top ==null) {
             return true;
         }
             return false;
-        
-
     }
+    
+    public int peek(){
+        return top.data;
+    }
+    public int count (){
+        int count=0;
+        Node temp = top;
+        while (temp != null) {
+            count ++;
+            temp = temp.next;
+        }
+        return count;
+    }
+   public boolean isFound(int item){
+    boolean found =false;
+       Node temp = top;
+       while (temp != null) {
+        if (item ==temp.data) {
+            return !found;
+           
+        }
+        temp = temp.next;
+       }
+       return found;
+   }
 }
 
 class Node {
