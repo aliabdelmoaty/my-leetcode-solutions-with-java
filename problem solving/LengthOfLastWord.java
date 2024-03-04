d6a4cae9fb24bc5a78c1da36a17f74e0fe399888
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-03-05 00:13:20 +0200

    Add LengthOfLastWord class to calculate the length of the last word in a string

---
+++ problem solving/LengthOfLastWord.java
@@ -0,0 +1,14 @@
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] strings = s.split(" ");
+        int last = strings.length - 1;
+        return strings[last].length();
+    }
+    
+
+    public static void main(String[] args) {
+        LengthOfLastWord lastWord = new LengthOfLastWord();
+        System.out.println(lastWord.lengthOfLastWord("Hello World"));
+    }
+}
+
