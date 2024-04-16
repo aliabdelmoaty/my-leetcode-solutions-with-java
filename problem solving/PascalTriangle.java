e662991a0550a926ce9e9cd178d2de659268277e
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-04-16 04:39:56 +0200

    Add PascalTriangle class for generating Pascal's triangle

--- problem solving/PascalTriangle.java
+++ problem solving/PascalTriangle.java
@@ -1,3 +1,32 @@
-public class PascalTriangle {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(Arrays.asList(1));
+        for (int i = 1; i < numRows; i++) { 
+            List<Integer> rows = new ArrayList<>();
+
+            for (int j = 0; j <= result.get(i - 1).size(); j++) {
+                if (j == 0 || j == result.get(i - 1).size()) {
+                    rows.add(1);
+
+                } else {
+                    rows.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+
+            }
+            result.add(rows);
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        PascalTriangle pTriangle = new PascalTriangle();
+        pTriangle.generate(6);
+    }
 }
