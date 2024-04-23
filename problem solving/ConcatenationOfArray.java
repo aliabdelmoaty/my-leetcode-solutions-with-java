73a7b6b290a2dd170f86215b008de674550bf0cf
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-04-23 02:13:19 +0200

    Refactor ConcatenationOfArray class to improve code readability and remove unnecessary print statement

--- problem solving/ConcatenationOfArray.java
+++ problem solving/ConcatenationOfArray.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class ConcatenationOfArray {
-    
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[nums.length + i] = nums[i];
+
+        }
+        System.out.println(Arrays.toString(ans));
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ConcatenationOfArray cArray = new ConcatenationOfArray();
+        int[] nums = { 1, 3, 2, 1 };
+        cArray.getConcatenation(nums);
+    }
 }
