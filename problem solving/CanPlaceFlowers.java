bae6888a1573e51dd8f7121fd4ea06658100eb53
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2024-04-25 22:47:47 +0200

    Refactor CanPlaceFlowers class to improve code readability and add test case

--- problem solving/CanPlaceFlowers.java
+++ problem solving/CanPlaceFlowers.java
@@ -1,3 +1,47 @@
+
 public class CanPlaceFlowers {
-    
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+
+        if (flowerbed.length > 1) {
+            for (int i = 0; i < flowerbed.length - 1; i++) {
+
+                if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                    flowerbed[i] = 1;
+                    count++;
+
+                }
+                if (i == flowerbed.length - 2 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                    flowerbed[i + 1] = 1;
+                    count++;
+
+                }
+                if (flowerbed[i] == 0 && flowerbed[i + 1] == 0 && flowerbed[i - 1] == 0
+                        && i != 0) {
+                    flowerbed[i] = 1;
+                    count++;
+
+                }
+            }
+        } else {
+            if (flowerbed[0] == 0) {
+                count++;
+
+            }
+
+        }
+
+        return count >= n;
+
+    }
+
+    public static void main(String[] args) {
+        CanPlaceFlowers cFlowers = new CanPlaceFlowers();
+        int[] flowerbed = { 0, 0, 1, 0, 0 };
+        int n = 1;
+        System.out.println(
+                cFlowers.canPlaceFlowers(flowerbed, n)
+
+        );
+    }
 }
